69be85fbba68e9e89cd6c2d4fff6864a28353337
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2020-11-19 14:10:08 -0600

    Se creo el proyecto y se agrego el metodo de ordenamiento burbuja

---
+++ src/Pruebas.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+class MetodosOrdenamientos{
+	//Agregar Menu!!!!
+	static class Burbuja{
+		static long tTnicio,tFin;
+		//Llenar con 1 millon de datos 
+		public static void ordenacionBurbuja1(int numeros[]) {
+			tTnicio=System.nanoTime();
+			for(int i=1;i<=numeros.length-1;i++) {
+				for(int j=0;j<numeros.length-i;j++) {
+					if(numeros[j]>numeros[j+1]) {
+						int aux=numeros[j];
+						numeros[j]=numeros[j+1];
+						numeros[j+1]=aux;
+					}//IF
+				}//FOR J
+			}//FOR I
+			tFin=System.nanoTime();
+			System.out.println("El tiempo de ejecucion en ordenamiento burbuja fue de "+(tFin-tTnicio));
+		}//Buebuja 1
+		/*
+		 * Cantidad de pasadas
+		 * Cantidad de comparaciones
+		 * Cantidad de intercambios
+		 */
+		//Hacer burbuja 2 y 3
+		//Tareas
+		public void ordenacionBurbuja2(int numeros[]) {}
+		
+		public void ordenacionBurbuja3(int numeros[]) {}
+		
+		public static void mostrarVector(int numeros[]) {
+			System.out.println(Arrays.toString(numeros));
+		}
+	}
+	
+}
+public class Pruebas {
+
+	public static void main(String[] args) {
+		System.out.println("-------Burbuja----------");
+		int numeros []= {6,2,9,7,1,0,4};
+		MetodosOrdenamientos.Burbuja.mostrarVector(numeros);
+		MetodosOrdenamientos.Burbuja.ordenacionBurbuja1(numeros);
+		System.out.println("Ordenado");
+		MetodosOrdenamientos.Burbuja.mostrarVector(numeros);
+		
+	}
+
+}
