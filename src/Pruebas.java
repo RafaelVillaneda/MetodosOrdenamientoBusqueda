4d150f7273a1b6068cefab357ceeaf2069e03b98
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2020-11-23 14:35:19 -0600

    Se agrego el codigo del metoo de ordenamiento por inserccion

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -81,8 +81,19 @@ class MetodosOrdenamientos{
 			System.out.println(Arrays.toString(numeros));
 		}
 	}
-	
+	public void ordenarInserccion(int [] numeros) {
+		int aux;
+		
+		for(int i=0;i<numeros.length;i++) {
+			aux=numeros[i];
+			for(int j=i-1;j>=0 &&numeros[j]>aux;j--) {
+				numeros[j+1]=numeros[j];
+				numeros[j]=aux;
+			}//Segundo for
+		}//Primer for
+	}//Metodo de ordenamiento INseccion
 }
+
 public class Pruebas {
 
 	public static void main(String[] args) {
@@ -127,7 +138,14 @@ public class Pruebas {
 			default:
 				break;
 			}
+			
 		}
+		System.out.println("----------------------------------------------");
+		int numeros1[]= {5,7,11,15,4,12,23,3,4,2,1,45,13};
+		System.out.println("Numeros desordenados: "+Arrays.toString(numeros1));
+		MetodosOrdenamientos orden=new MetodosOrdenamientos();
+		orden.ordenarInserccion(numeros1);
+		System.out.println("Numeros ordenados: "+Arrays.toString(numeros1));
 		
 	}
 
